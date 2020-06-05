bd38b3b38b31bd4ce87e9b4090607983d3188b13
Evandro Jun Ishikawa <evandroishikawa@usp.br>
2020-06-05 03:18:57 -0300

    create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.svg';
+
+import './styles.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <div id="page-home">
+        <div className="content">
+          <header>
+            <img src={logo} alt="Ecoleta" />
+          </header>
+
+          <main>
+            <h1>Seu marketplace de coleta de resíduos.</h1>
+            <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
+
+            <Link to="/cadastro/ponto-de-coleta">
+              <span>
+                <FiLogIn />
+              </span>
+
+              <strong>Cadastre um ponto de coleta</strong>
+            </Link>
+          </main>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
